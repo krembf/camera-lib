995294bd60c1cc58eb7f21ba161849c37a7d9c7d
Boris Kreminski <krembf@gmail.com>
2019-08-23 01:43:49 -0400

    minor changes to electron-quick-start

--- electron-quick-start/renderer.js
+++ electron-quick-start/renderer.js
@@ -3,6 +3,16 @@
 // All of the Node.js APIs are available in this process.
 console.log("Renderer Enter");
 
+const SerialPort = require('serialport')
+const MockBinding = require('@serialport/binding-mock')
+const Readline = require('@serialport/parser-readline')
+const path = '/dev/ROBOT';
+SerialPort.Binding = MockBinding
+// Create a port and enable the echo and recording.
+MockBinding.createPort(path, { echo: true, record: true, readyData: "FREDDY" })
+const port = new SerialPort(path)
+
+
 var addon = require('bindings')('cameraaddon.node');
 
 document.body.onload = addElement;
@@ -18,4 +28,18 @@ function addElement () {
   // add the newly created element and its content into the DOM 
   var currentDiv = document.getElementById("div1"); 
   document.body.insertBefore(newDiv, currentDiv); 
+
+  const parser = new Readline()
+  port.pipe(parser)
+
+  parser.on('data', line => console.log(`> ${line}`))
+  port.write('ROBOT POWER ON\n')
+}
+
+function getData() {
+  console.log("getData clicked");
+}
+
+let getData2 = function() {
+  console.log("getData clicked");
 }
